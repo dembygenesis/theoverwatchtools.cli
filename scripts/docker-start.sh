91fd82e87ecd01fa8592e4c93cc71705cf09b618
Demby <dmcchy@gmail.com>
2024-08-04 16:17:49 +0300

    some progress

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,1 +1,3 @@
-docker-compose -p theoverwatchtools -f ./docker/docker-compose.yaml up --force-recreate --build
+# docker-compose -p theoverwatchtools -f ./docker/docker-compose.yaml up --force-recreate --build
+# I wonder what'll happen if I don't force-create
+docker-compose -p theoverwatchtools -f ./docker/docker-compose.yaml up
