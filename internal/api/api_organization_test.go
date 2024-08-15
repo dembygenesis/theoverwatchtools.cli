b2caa13fdb218aa698a250a97eeb43e4108bba15
lawrence michael margaja <lawrencemichaelmargaja@lawrences-MacBook-Air.local>
2024-08-16 02:37:59 +0800

    refined test for listOrganizations on api layer.

--- internal/api/api_organization_test.go
+++ internal/api/api_organization_test.go
@@ -3,12 +3,10 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/dembygenesis/local.tools/internal/api/testassets"
 	"github.com/dembygenesis/local.tools/internal/lib/logger"
 	"github.com/dembygenesis/local.tools/internal/model"
 	"github.com/dembygenesis/local.tools/internal/persistence/database_helpers/mysql/mysqlhelper"
-	"github.com/dembygenesis/local.tools/internal/persistence/database_helpers/mysql/mysqltx"
 	"github.com/dembygenesis/local.tools/internal/utilities/strutil"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
@@ -44,9 +42,8 @@ func getTestCasesListOrganizations() []testCaseListOrganization {
 					UserId: 1,
 				}
 
-				value, err := modules.OrganizationService.AddOrganization(context.Background(), organizationModel)
+				_, err = modules.OrganizationService.AddOrganization(context.Background(), organizationModel)
 				require.NoError(t, err, "error adding the organization")
-				fmt.Println("the value ---- ", strutil.GetAsJson(value))
 
 				err = tx.Commit()
 				require.NoError(t, err, "unexpected error committing the transaction")
@@ -78,27 +75,13 @@ func getTestCasesListOrganizations() []testCaseListOrganization {
 func Test_ListOrganizations(t *testing.T) {
 	for _, testCase := range getTestCasesListOrganizations() {
 		t.Run(testCase.name, func(t *testing.T) {
-			db, cp, cleanup := mysqlhelper.TestGetMockMariaDB(t)
+			db, _, cleanup := mysqlhelper.TestGetMockMariaDB(t)
 			defer cleanup()
-
-			require.NotEmpty(t, testCase.name, "unexpected empty test name")
-
 			if testCase.queryParameters == nil {
 				testCase.queryParameters = make(map[string]interface{})
 			}
 
 			handlers, _ := testCase.getContainer(t)
-
-			txHandler, err := mysqltx.New(&mysqltx.Config{
-				Logger:       logger.New(context.TODO()),
-				Db:           db,
-				DatabaseName: cp.Database,
-			})
-			require.NoError(t, err, "unexpected error creating the tx handler")
-
-			txHandlerDb, err := txHandler.Db(context.TODO())
-			require.NoError(t, err, "unexpected error fetching the db from the tx handler")
-			require.NotNil(t, txHandlerDb, "unexpected nil tx handler db")
 
 			cfg := &Config{
 				BaseUrl:             testassets.MockBaseUrl,
@@ -106,38 +89,26 @@ func Test_ListOrganizations(t *testing.T) {
 				CategoryService:     handlers.CategoryService,
 				OrganizationService: handlers.OrganizationService,
 				Logger:              logger.New(context.TODO()),
-				TxHandler:           txHandlerDb,
 			}
-
-			testCase.mutations(t, db, handlers)
 
 			api, err := New(cfg)
 			require.NoError(t, err, "unexpected error instantiating api")
 			require.NotNil(t, api, "unexpected api nil instance")
 
 			url := strutil.AppendQueryToURL("/api/v1/organization", testCase.queryParameters)
-			fmt.Printf("Constructed URL: %s\n", url)
 			req := httptest.NewRequest(http.MethodGet, url, nil)
 			req.Header = map[string][]string{
 				"Content-Type":    {"application/json"},
 				"Accept-Encoding": {"gzip", "deflate", "br"},
 			}
 
-			fmt.Printf("Request: %+v\n", req)
+			testCase.mutations(t, db, handlers)
 
 			resp, err := api.app.Test(req, 100)
 			require.NoError(t, err, "unexpected error executing test")
 
 			respBytes, err := io.ReadAll(resp.Body)
 			require.Nil(t, err, "unexpected error reading the response")
-
-			var responseMap map[string]interface{}
-			if err := json.Unmarshal(respBytes, &responseMap); err != nil {
-				t.Fatalf("failed to unmarshal response: %v", err)
-			}
-
-			fmt.Println("Response --------------- ", strutil.GetAsJson(responseMap))
-
 			testCase.assertions(t, respBytes, resp.StatusCode)
 		})
 	}
